5575cc08b6fcb506e5871e8f1709ae39e6e4a7a4
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-10-29 01:09:23 +0600

    all packages installed

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA97R8V_DRR8ujWRizFRPcEVE7HK4VQRKQ",
+    authDomain: "tourism-website-samimeadad.firebaseapp.com",
+    projectId: "tourism-website-samimeadad",
+    storageBucket: "tourism-website-samimeadad.appspot.com",
+    messagingSenderId: "688544046",
+    appId: "1:688544046:web:047795be3025e7af6fc2bf"
+};
+
+export default firebaseConfig;
