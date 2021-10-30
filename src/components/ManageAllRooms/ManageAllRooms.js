382e56567ea4d63f55594ea342eb5a0457c25f36
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-10-30 22:51:55 +0600

    delete function implemented

--- src/components/ManageAllRooms/ManageAllRooms.js
+++ src/components/ManageAllRooms/ManageAllRooms.js
@@ -1,11 +1,29 @@
-import React from 'react';
 import { Button, Table } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 import useRooms from '../../Hooks/useRooms';
 
 const ManageAllRooms = () => {
-    const [ rooms ] = useRooms();
+    const [ rooms, setRooms ] = useRooms();
+
+    //DELETE a Room
+    const handleDeleteRoom = id => {
+        const proceed = window.confirm( "Are you sure to delete the Room data?" );
+        if ( proceed ) {
+            const url = `https://guarded-peak-27154.herokuapp.com/rooms/${ id }`;
+            fetch( url, {
+                method: 'DELETE'
+            } )
+                .then( res => res.json() )
+                .then( data => {
+                    if ( data.deletedCount > 0 ) {
+                        alert( 'Room Data Deleted Successfully' );
+                        const remainingRooms = rooms.filter( room => parseInt( room._id ) !== parseInt( id ) );
+                        setRooms( remainingRooms );
+                    }
+                } )
+        }
+    }
 
     return (
         <div className="text-center my-5 w-50 mx-auto">
@@ -24,7 +42,7 @@ const ManageAllRooms = () => {
                             rooms.map( room => <tr>
                                 <td>{ room.type }</td>
                                 <td><Button className="btn btn-success ms-3"><FontAwesomeIcon icon={ faEdit } /></Button></td>
-                                <td><Button className="btn btn-danger ms-3"><FontAwesomeIcon icon={ faTrash } /></Button></td>
+                                <td><Button onClick={ () => handleDeleteRoom( room._id ) } className="btn btn-danger ms-3"><FontAwesomeIcon icon={ faTrash } /></Button></td>
                             </tr> )
                         }
 
