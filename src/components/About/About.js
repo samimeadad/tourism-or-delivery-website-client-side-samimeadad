592ac14f298522b36320caa2a3a284e48d890f8a
samimeadad <sami.meadad@gmail.com>
2021-10-31 12:32:51 +0600

    about us page done Alhamdulillah

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h2>About Page</h2>
+        <div className="container my-5">
+            <h1 className="mb-5 text-center text-primary fw-bold">About Bayview</h1>
+            <hr />
+            <p className="fs-5">
+                Bayview Hotel & Resort is a privately owned 5 Star Standard Luxury Hotel & Resort in Cox's Bazar, Bangladesh. It is situated in an attractive location of Marine Drive Road, Cox's Bazar. It is a place of choice of most of the corporate executives, foreigners, newly married couples, and for those who prefer to stay in luxury with stunning view of The 'Bay of Bengal'. <br /><br />
+
+                The Hall of Stars has the capacity of 900 persons as well as we have another 3 (three) different capacitated halls, Andromeda, Level-5 and Board Meeting Room which are also ideal for business conference and musical party. The Conference Center is fully equipped with a wide range of presentation equipments; multimedia projector, overhead projector, slide projector, projection screen, comprehensive sound system with coat and wireless microphones, TV, computers and so on. <br /><br />
+
+                Apart from the above services we have other value added services that ensure the Five Star satisfaction to our honorable clients. We provide 24 hours' Front Desk & Room services, business & secretarial services, laundry & valet services, Multi-cuisine Restaurant, BBQ restaurant, Coffee & Pastry shop, Pool terrace, Swimming pool, Gym, Spa, Sauna, Fitness center, Billiard & Snooker room, Gift shop 24 hours CC TV, emergency doctor and limousine service. Besides, we could provide more than 200 cars parking to our respectable guests. Therefore, we are looking forward to become your hospitality partner as we can ensure you a lavish accommodation, luxury comfort, extensive security, world class cuisine, contemporary conference facilities as well as splendid sea view of Cox's Bazar.
+            </p>
         </div>
     );
 };
